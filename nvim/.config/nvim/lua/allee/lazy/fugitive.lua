e7bda03151cc3b5b3417469f43e7ac5a0cf8ebfd
Allan Lee <allanlee@Allans-MacBook-Pro.local>
2025-03-10 19:12:18 -0500

    Updated fugitive command

--- nvim/.config/nvim/lua/allee/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/allee/lazy/fugitive.lua
@@ -32,7 +32,7 @@ return {
         })
 
 
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
     end
 }
