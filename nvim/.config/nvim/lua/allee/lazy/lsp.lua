3d91042a6ac2319b96867d830d8ea7ffbacc1689
Allan Lee <allanlee@Allans-MacBook-Pro.local>
2025-03-11 08:51:29 -0500

    update

--- nvim/.config/nvim/lua/allee/lazy/lsp.lua
+++ nvim/.config/nvim/lua/allee/lazy/lsp.lua
@@ -1,107 +1,120 @@
 return {
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    config = function()
-      local cmp = require('cmp')
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        config = function()
+            local cmp = require('cmp')
 
-      cmp.setup({
-        sources = {
-          {name = 'nvim_lsp'},
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-y>'] = cmp.mapping.confirm({ select=true }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-      })
-    end
-  },
-
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    cmd = 'LspInfo',
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                }),
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+            })
+        end
     },
-    init = function()
-      -- Reserve a space in the gutter
-      -- This will avoid an annoying layout shift in the screen
-      vim.opt.signcolumn = 'yes'
-    end,
-    config = function()
-      local lsp = require('lspconfig')
-      local lsp_defaults = lsp.util.default_config
 
-      -- Add cmp_nvim_lsp capabilities settings to lspconfig
-      -- This should be executed before you configure any language server
-      lsp_defaults.capabilities = vim.tbl_deep_extend(
-        'force',
-        lsp_defaults.capabilities,
-        require('cmp_nvim_lsp').default_capabilities()
-      )
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = 'LspInfo',
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+        },
+        init = function()
+            -- Reserve a space in the gutter
+            -- This will avoid an annoying layout shift in the screen
+            vim.opt.signcolumn = 'yes'
+        end,
+        config = function()
+            local lsp = require('lspconfig')
+            local lsp_defaults = lsp.util.default_config
 
-      -- These are just examples. Replace them with the language
-      -- servers you have installed in your system
-      lsp.gopls.setup({})
-      lsp.bashls.setup({})
-      lsp.yamlls.setup({})
-      lsp.lua_ls.setup({})
-      lsp.pyright.setup({})
-      lsp.jdtls.setup({})
-    end
-  },
-  -- Mason
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim"
-    },
-    config = function()
-      local mason = require("mason")
-      
-      mason.setup({
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            -- This should be executed before you configure any language server
+            lsp_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+            lsp.pyright.setup {
+                settings = {
+                    pyright = {
+                        -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            -- Ignore all files for analysis to exclusively use Ruff for linting
+                            ignore = { '*' },
+                        },
+                    },
+                },
             }
-        }
-      })
+            -- These are just examples. Replace them with the language
+            -- servers you have installed in your system
+            lsp.gopls.setup({})
+            lsp.bashls.setup({})
+            lsp.yamlls.setup({})
+            lsp.lua_ls.setup({})
+            lsp.pyright.setup({})
+            lsp.jdtls.setup({})
+        end
+    },
+    -- Mason
+    {
+        "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim"
+        },
+        config = function()
+            local mason = require("mason")
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
 
-      -- local mason_config = require("mason-lspcon")
-      -- local capabilities = vim.tbl_deep_extend(
-      --       "force",
-      --       {},
-      --       vim.lsp.protocol.make_client_capabilities(),
-      --       cmp_lsp.default_capabilities())
+            -- local mason_config = require("mason-lspcon")
+            -- local capabilities = vim.tbl_deep_extend(
+            --       "force",
+            --       {},
+            --       vim.lsp.protocol.make_client_capabilities(),
+            --       cmp_lsp.default_capabilities())
 
-      -- mason_config.setup({
-      --   ensure_installed = {
-      --     "lua_ls",
-      --     "rust_analyzer",
-      --     "gopls",
-      --   },
-      --   handlers = {
-      --     function(server_name) -- default handler (optional)
-      --       require("lspconfig")[server_name].setup {
-      --           capabilities = capabilities
-      --       }
-      --   end,
-      --   }
-      -- })
-    end
-  }
+            -- mason_config.setup({
+            --   ensure_installed = {
+            --     "lua_ls",
+            --     "rust_analyzer",
+            --     "gopls",
+            --   },
+            --   handlers = {
+            --     function(server_name) -- default handler (optional)
+            --       require("lspconfig")[server_name].setup {
+            --           capabilities = capabilities
+            --       }
+            --   end,
+            --   }
+            -- })
+        end
+    }
 }
+
--- nvim/.config/nvim/lua/allee/lazy/telescope.lua
+++ nvim/.config/nvim/lua/allee/lazy/telescope.lua
@@ -18,15 +18,23 @@ return {
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sa', builtin.live_grep, { desc = '[S]earch by [A]ll Grep' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffer' })
         vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]ithub' })
 
+
+        vim.keymap.set('n', '<leader>s/', function()
+            builtin.live_grep {
+                grep_open_files = true,
+                prompt_title = 'Live Grep in Open Files',
+            }
+        end, { desc = '[S]earch [/] in Open Files' })
+
         vim.keymap.set('n', '<leader>sn', function()
-            builtin.find_files { cwd = vim.fn.stdpath('config')}
+            builtin.find_files { cwd = vim.fn.stdpath('config') }
         end, { desc = '[S]earch [N]eovim files' })
         -- vim.keymap.set('n', '<leader>pws', function()
         --     local word = vim.fn.expand("<cword>")
@@ -36,8 +44,8 @@ return {
         --     local word = vim.fn.expand("<cWORD>")
         --     builtin.grep_string({ search = word })
         -- end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>sp', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ")})
+        end, { desc = '[S]earch [P]roject' })
     end
 }
